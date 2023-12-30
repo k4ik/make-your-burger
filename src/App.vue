73007d170b70b0bc7b73380bacb4dac5d599f159
k4ik <kaikleal1234@gmail.com>
2023-12-29 22:41:31 -0300

    :sparkles: feat: components

---
+++ src/App.vue
@@ -0,0 +1,49 @@
+<template>
+  <div>
+    <Navbar :logo="logo_src" :alt="app_name" />
+    <RouterView />
+    <Footer />
+  </div>
+</template>
+
+<script>
+  import { RouterView } from 'vue-router'
+  import Navbar from './components/Navbar.vue'
+  import Footer from './components/Footer.vue'
+
+  export default {
+    name: 'App',
+    components: {
+      Navbar,
+      RouterView,
+      Footer
+    },
+    data() {
+      return {
+        logo_src: "/images/logo.png",
+        app_name: "Make your Burger"
+      }
+    }
+  }
+</script>
+
+<style>
+  * {
+    font-family: "Helvetica";
+    padding: 0;
+    margin: 0;
+    box-sizing: border-box;
+  }
+
+  .main-container {
+    margin: 50px;
+    min-height: 450px;
+  }
+
+  h1 {
+    text-align: center;
+    font-size: 42px;
+    margin-bottom: 30px;
+    color: #222;
+  }
+</style>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,52 @@
+<script>
+  import { RouterLink } from 'vue-router'
+
+  export default {
+    name: "Navbar",
+    components: {
+      RouterLink
+    },
+    props: ["logo", "alt"]
+  }
+</script>
+
+<template>
+  <header>
+    <nav id="nav">
+      <RouterLink to="/" id="logo-url"><img :src="logo" :alt="alt" id="logo" /></RouterLink>
+      <RouterLink to="/">Home</RouterLink>
+      <RouterLink to="/pedidos">Pedidos</RouterLink>
+    </nav>
+  </header>
+</template>
+
+<style scoped>
+    #nav {
+        background-color: #222;
+        border-bottom: 4px solid #111;
+        padding: 15px 50px;
+        display: flex;
+        justify-content: flex-end;
+        align-items: center;
+    }
+    
+    #nav a {
+        color: #fcba03;
+        text-decoration: none;
+        margin: 12px;
+        transition: .5s;
+    }
+
+    #nav a:hover {
+        color: #fff;
+    }
+
+    #nav #logo-url {
+        margin: auto;
+        margin-left: 0;
+    }
+
+    #logo {
+        width: 40px;
+    }
+</style>
