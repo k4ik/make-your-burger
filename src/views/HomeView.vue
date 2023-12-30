6c102dc6184c9f1d8a962d9acf744b2ca0da0281
k4ik <kaikleal1234@gmail.com>
2023-12-29 22:41:56 -0300

    :sparkles: feat: routers

---
+++ src/views/HomeView.vue
@@ -0,0 +1,20 @@
+<script>
+  import Banner from "../components/Banner.vue"
+  import BurgerForm from "../components/BurgerForm.vue"
+
+  export default {
+    name: "Home",
+    components: {
+      Banner,
+      BurgerForm
+    }
+  }
+</script>
+
+<template>
+  <Banner />
+  <main class="main-container">
+    <h1>Monte o seu Burger:</h1>
+    <BurgerForm />
+  </main>
+</template>
---
+++ src/views/PedidosView.vue
@@ -0,0 +1,21 @@
+<template>
+  <main class="main-container">
+    <h1>Gerenciar pedidos:</h1>
+    <Dashboard />
+  </main>
+</template>
+
+<script>
+  import Dashboard from "../components/Dashboard.vue";
+
+  export default {
+    name: "Pedidos",
+    components: {
+      Dashboard
+    }
+  };
+</script>
+
+<style>
+
+</style>
